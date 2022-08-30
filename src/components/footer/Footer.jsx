cf5ab45d25aa969ea006f59a8b8dc5038965a9f2
youngjerald239 <youngjerald239@gmail.com>
2022-08-30 12:56:18 -0500

    added information and art

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,13 +6,13 @@ import {FaItchIo} from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className="footer_logo">Jerald</a>
+      <a href="#" className="footer_logo">MystoriArt</a>
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#portfolio">Projects</a></li>
-        <li><a href="#testimonial">Games</a></li>
+        <li><a href="#testimonial">Fanart</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
 
