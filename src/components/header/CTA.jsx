cdaa296d823f60749ca4305dc6b905e45fbee6a0
youngjerald239 <youngjerald239@gmail.com>
2022-08-31 10:06:40 -0500

    adjusted header links

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/CV.pdf'
+
 
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href="https://ko-fi.com/mystoriart" download className='btn'>Donate Ko-fi</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
